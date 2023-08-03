d19b1ea155001276dfe00d420618e07f13b95f97
lexjarvis <116358840+lexjarvis@users.noreply.github.com>
2023-08-03 17:18:48 -0400

    Styling updates

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -1,63 +1,8 @@
-// import React, { useState } from 'react';
-// import Spinner from './Spinner';
-// import EventPopup from './EventPopup';
-// import axios from 'axios';
-// import './Homepage.css'; // Import the CSS file
-
-// const Homepage = () => {
-//   const [location, setLocation] = useState('');
-//   const [showPopup, setShowPopup] = useState(false);
-//   const [eventData, setEventData] = useState({});
-
-//   const handleLocationChange = (e) => {
-//     setLocation(e.target.value);
-//   };
-
-//   const handleSpinClick = () => {
-//     axios
-//       .post('http://localhost:8008/spin', { location }) // Adjust the URL to your backend server URL
-//       .then((response) => {
-//         setEventData(response.data);
-//         setShowPopup(true);
-//       })
-//       .catch((error) => {
-//         console.error('Error fetching event:', error);
-//         // Optionally, show an error message to the user.
-//       });
-//   };
-
-//   const handleClosePopup = () => {
-//     setShowPopup(false);
-//   };
-
-//   return (
-//     <div className="homepage">
-//       <header>
-//         {/* Your logo here */}
-//         <h1>EventSpin</h1>
-//       </header>
-//       <div className="body">
-//         <input
-//           type="text"
-//           placeholder="Enter your location..."
-//           value={location}
-//           onChange={handleLocationChange}
-//         />
-//         <button onClick={handleSpinClick}>Spin</button>
-//         <Spinner />
-//       </div>
-//       {showPopup && <EventPopup eventData={eventData} onClose={handleClosePopup} />}
-//     </div>
-//   );
-// };
-
-// export default Homepage;
-
 import React, { useState } from 'react';
 import Spinner from './Spinner';
 import EventPopup from './EventPopup';
 import axios from 'axios';
-import './Homepage.css'; // Import the CSS file
+import './Homepage.css'; 
 
 const Homepage = () => {
   const [location, setLocation] = useState('');
@@ -78,7 +23,7 @@ const Homepage = () => {
         console.error('Error fetching event:', error);
       })
       .finally(() => {
-        setShowPopup(true); // Show the popup after the API request is completed
+        setShowPopup(true);
       });
   };
 
@@ -89,8 +34,7 @@ const Homepage = () => {
   return (
     <div className="homepage">
       <header>
-        {/* Your logo here */}
-        <h1>EventSpin</h1>
+        <h1>Event Spin</h1>
       </header>
       <div className="body">
         <input
--- frontend/src/Spinner.js
+++ frontend/src/Spinner.js
@@ -8,22 +8,14 @@ const Spinner = ({ onSpin }) => {
     setIsSpinning(true);
     setTimeout(() => {
       setIsSpinning(false);
-      onSpin(); // Call the onSpin callback after the spin animation is complete
-    }, 3000); // Simulate a 3-second spin duration, adjust as needed
+      onSpin(); 
+    }, 3000); 
   };
 
   return (
     <div className="spinner">
       <div className={`wheel ${isSpinning ? 'spinning' : ''}`} onClick={handleSpin}>
-        {/* <div className="triangle"></div>
-        <div className="triangle"></div>
-        <div className="triangle"></div>
-        <div className="triangle"></div>
-        <div className="triangle"></div>
-        <div className="triangle"></div>
-        <div className="triangle"></div>
-        <div className="triangle"></div> */}
-      <div className="spin-button" onClick={handleSpin}>Spin</div>
+      <div className="spin-button" onClick={handleSpin}>SPIN</div>
       </div>
     </div>
   );
